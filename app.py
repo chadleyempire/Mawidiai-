6c3a51994f8e2c30c2655ad131f7142d4a477003
Chadley Empire <EMAIL_YAKO_HAPA>
2026-07-05 01:41:20 +0300

    final fix

--- app.py
+++ app.py
@@ -1,16 +1,10 @@
-from http.server import BaseHTTPRequestHandler, HTTPServer
+from flask import Flask
 
-class MyHandler(BaseHTTPRequestHandler):
-    def do_GET(self):
-        self.send_response(200)
-        self.send_header('Content-type', 'text/html')
-        self.end_headers()
+app = Flask(__name__)
 
-        with open("MawidyAPK/app/index.html", "r") as f:
-            page = f.read()
+@app.route("/")
+def home():
+    return "🤖 Mawidy AI iko online!"
 
-        self.wfile.write(page.encode())
-
-server = HTTPServer(('0.0.0.0', 8080), MyHandler)
-print("Server running at http://localhost:8080")
-server.serve_forever()
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
